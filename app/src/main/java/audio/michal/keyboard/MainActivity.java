36ccf4a17fdd4d86c39384a57ecb8734bdf0b137
Magduss <magduss@michal.audio>
2017-11-19 16:21:59 +0100

    Added button for website opening

--- app/src/main/java/audio/michal/keyboard/MainActivity.java
+++ app/src/main/java/audio/michal/keyboard/MainActivity.java
@@ -1,7 +1,9 @@
 package audio.michal.keyboard;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.GridView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
@@ -23,9 +25,19 @@ public class  MainActivity extends AppCompatActivity {
         KeyboardLayoutGenerator.fillKeyboard(ll);
         List<KeyModel> list = new ArrayList<>();
         for (int i=0; i<2; i++){
-            for (KeyModel.Tone t: KeyModel.Tone.values()){
-//                KeyModel key = new KeyModel(i,)
-            }
+            // for (KeyModel.Tone t: KeyModel.Tone.values()){
+            //     KeyModel key = new KeyModel(i,)
+            // }
         }
     }
+
+    public void openWebsite (View view){
+        String webAddress = "@string/address";
+        Intent intent = new Intent (Intent.ACTION_OPEN_DOCUMENT);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, webAddress);
+        startActivity(intent);
+
+    }
+
 }
